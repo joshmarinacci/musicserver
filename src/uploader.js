6294382edf6b626b0cfdf4cc599383130529fb68
josh <joshua@marinacci.org>
2018-04-02 21:22:09 -0700

    make upload script work

--- src/uploader.js
+++ src/uploader.js
@@ -1,7 +1,8 @@
-console.log(process.argv)
-const fs = require('fs');
+const fs = require('fs')
+const path = require('path')
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-if(process.argv.length !== 4) printUsage()
+if(process.argv.length !== 4) return printUsage()
 
 const server = process.argv[2]
 const dirname = process.argv[3]
@@ -13,13 +14,32 @@ Uploader: uploads a directory of files to the specified server
 Usage:
    node src/uploader <serveraddress> <dirname>
 Example:
-   node src/uploader http://localhost:6688/ ~/Music/iTunes/iTunes\\ Media/Music/Adele/25
+   node src/uploader http://localhost:9872/ ~/Music/iTunes/iTunes\\ Media/Music/Adele/25
 `)
 }
 
-function uploadFiles(dirname) { fs.readdirSync(dirname).filter(onlyMP3Files).map(uploadFile) }
+function onlyMP3Files(name) {
+    return (name.indexOf(".mp3")>0)
+}
 
-function uploadFile(filename) {
-    console.log("uploading the file",filename)
+function uploadFiles(dirname) {
+    fs.readdirSync(dirname)
+        .filter(onlyMP3Files)
+        .map((filename)=>uploadFile(path.join(dirname,filename)))
+}
+
+function uploadFile(filepath) {
+    console.log("uploading the file",filepath)
+    const url = `${server}api/songs/upload/some-file`
+    console.log(`uploading ${url}`)
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load',() => {
+        console.log("loaded",xhr.responseText)
+    })
+    xhr.addEventListener('error',()=>{
+        console.log("error")
+    })
+    xhr.open('POST',url)
+    xhr.send(fs.readFileSync(filepath))
 }
 
